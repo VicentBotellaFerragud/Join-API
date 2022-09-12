791bb677dc81444eab2fa5a089c7980143a35779
Alexandru-Andrei Todor <at@interap.de>
2022-09-12 08:36:57 +0200

    login test added

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
 
 # Create your tests here.
+
+class LoginTest(TestCase):
+    def testLogin(self):
+        self.client = Client()
+        self.user = User.objects.create_user('test_user', password='test_user')
+        self.client.login(username='test_user', password='test_user')
+        
